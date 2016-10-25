f896c8814dad0466457945b7da162d950471a8f1
Brian Wachanga <brian@talicraft.com>
2016-10-25 12:04:45 +0300

    It has begun

---
+++ README.md
@@ -0,0 +1,32 @@
+# Interactive story telling
+This is a project for generating / embedding interactive graphics ( various kinds of charts, counters, animated html videos ) in PesaCheck's medium blog, and use a dashboard story-telling approach. This repo may / may not graduate and become PesaCheck's new website.
+
+This project is heavily inspired by https://github.com/CodeForAfrica/AfricanSpending.ComparativeScales.git
+
+### Dependencies
+This project is implemented in pure javascript, using Angular, Bootstrap and Chart.js as the core dependencies
+
+### Getting started
+[Install nodejs](https://nodejs.org/en/download/)
+Clone the project
+```
+git clone git@github.com:PesaCheck/interactive-visualization.git
+cd path/to/cloned/folder
+```
+
+### Install dependencies
+```
+npm install
+```
+### Start dev server
+```
+gulp
+```
+Access the development site using the link
+(http://localhost:6271)
+
+### Build
+```
+gulp build
+```
+The build can be accessed from the public folder
---
+++ gulpfile.js
@@ -0,0 +1,111 @@
+// include gulp
+var gulp = require('gulp');
+
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var flatten = require('gulp-flatten');
+var gulpUtil = require('gulp-util');
+var webserver = require('gulp-webserver');
+var ngConstant = require('gulp-ng-constant');
+
+var PATHS = {
+  node: "./node_modules/"
+}
+
+var deps = {
+  js: [
+    PATHS.node + "angular/angular.js",
+    PATHS.node + "chart.js/dist/Chart.js"
+  ],
+  css: [
+    PATHS.node + "bootstrap/dist/css/bootstrap.css",
+    PATHS.node + "bootstrap/dist/css/bootstrap.css.map"
+  ],
+  fonts: [
+    PATHS.node + "boostrap/dist/fonts/**/**"
+  ]
+}
+
+// build JS library dependencies
+gulp.task("js-dependencies", function(){
+   gulp.src(deps.js)
+    .pipe(concat('pesacheck.deps.js'))
+    // .pipe(uglify()).on('error', gulpUtil.log)
+    .pipe(gulp.dest('./public/js/'))
+
+});
+// build CSS library dependencies
+gulp.task("css-dependencies", function(){
+   gulp.src(deps.css)
+    .pipe(concat('pesacheck.deps.css'))
+    .pipe(gulp.dest('./public/css/'))
+})
+
+gulp.task("fonts", function(){
+  gulp.src(deps.fonts)
+    .pipe(gulp.dest('./public/fonts'))
+})
+
+// main app files
+gulp.task("main", function(){
+   gulp.src([
+       "./index.html",
+       "./app.js",
+       "./package.json"
+   ])
+   .pipe(gulp.dest("./public/"));
+});
+
+// build html template files
+gulp.task("templates", function(){
+  gulp.src("templates/**/**.html")
+    .pipe(flatten())
+    .pipe(gulp.dest("./public/tpls"))
+});
+
+// build js modules
+gulp.task("js-modules", function(){
+  gulp.src("js/**/**.js")
+    .pipe(concat('pesacheck.js'))
+    .pipe(gulp.dest('./public/js'))
+})
+
+// build css modules
+gulp.task("css-modules", function(){
+  gulp.src("sass/**/**.scss")
+    .pipe(sass().on('error', sass.logError))
+    .pipe(concat('pesacheck.css'))
+    .pipe(gulp.dest('./public/css'))
+})
+
+// serve a build
+gulp.task('webserver', function(){
+  gulp.src('dist')
+   .pipe(webserver({
+     livereload: true,
+     open: true,
+     fallback: 'index.html',
+     port: 6271
+   }));
+});
+
+// watch for file changes
+gulp.task('watch', function(){
+  return gulp.watch(['./**/**.**', '!./dist/**'], ['build'])
+});
+
+// build website
+gulp.task('build', [
+  'js-dependencies',
+  'css-dependencies',
+  'fonts',
+  'main',
+  'templates',
+  'js-modules',
+  'css-modules'
+])
+
+// run development
+// default task
+gulp.task('default', ['build','webserver','watch'])

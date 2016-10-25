82dbcf5e48eaa79b5f085c9dcaccafa03a736188
Brian Wachanga <brian@talicraft.com>
2016-10-25 20:05:20 +0300

    second trial subtree commit

---
+++ public/js/pesacheck.js
@@ -0,0 +1,58 @@
+
+
+
+pesacheck.directive("pesacheckSlider", [
+  function(){
+    return {
+      restrict: "E",
+      replace: false,
+      templateUrl: "tpls/pesacheck-slider.html",
+      controller: "PesaCheckSliderCtrl",
+      controllerAs: "PCSC"
+    }
+  }
+]);
+
+pesacheck.factory("$PesaCheckSlider", function(){
+  var Slider = {
+    content: [
+      {
+        image: "./images/articles/orphans-and-disabled.jpeg",
+        text: ""
+      },
+      {
+        image: "./images/articles/water-for-schools.jpeg",
+        text: ""
+      }
+    ],
+    setContent: function(content){
+      Slider.content = content;
+    }
+  }
+  return Slider;
+});
+
+
+pesacheck.controller("PesaCheckSliderCtrl",[
+  "$PesaCheckSlider","$scope",
+  function($PesaCheckSlider, $scope){
+    var self = this;
+
+    angular.extend(self, {
+      content: $PesaCheckSlider.content,
+      interval: 5000
+    })
+
+    console.log("pesacheck slider ")
+
+    $scope.$watch(
+      function(){
+        return $PesaCheckSlider.content
+      },
+      function(newVal, oldVal){
+        if(oldVal != newVal)
+          self.content = newVal;
+      }
+    )
+  }
+])

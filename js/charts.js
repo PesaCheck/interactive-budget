f6da3f6529ee17cd6a099062554101ce0aba31ae
Brian Wachanga <brian@talicraft.com>
2016-10-31 09:38:10 +0300

    Using google visualization APIs because it has more options

--- js/charts.js
+++ js/charts.js
@@ -6,17 +6,49 @@ pesacheck.directive("pesacheckCharts", [
       templateUrl: "./tpls/pesacheck-charts.html",
       controller: "pesacheckChartsCtrl",
       link: function(scope, element, attrs, controller, transcludeFn){
-        console.log(attrs.type)
-        scope.height = attrs.height;
-        scope.width = attrs.width;
+        // Load the Visualization API and the corechart package.
+        google.charts.load('current', {'packages':['corechart']});
 
-        if(attrs.type == undefined){
-          return;
-        } else if(attrs.type == 'bubbleChart'){
-          var myBubbleChart = new Chart(element, controller.bubbleChartCfg);
-        } else if(attrs.type == 'barChart'){
-          var myBubbleChart = new Chart(element, controller.barChartCfg);
+        // Set a callback to run when the Google Visualization API is loaded.
+        google.charts.setOnLoadCallback(drawChart);
+
+        // Callback that creates and populates a data table,
+        // instantiates the pie chart, passes in the data and
+        // draws it.
+        function drawChart() {
+
+          // Create the data table.
+          var data = new google.visualization.DataTable();
+          data.addColumn('string', 'Topping');
+          data.addColumn('number', 'Slices');
+          data.addRows([
+            ['Mushrooms', 3],
+            ['Onions', 1],
+            ['Olives', 1],
+            ['Zucchini', 1],
+            ['Pepperoni', 2]
+          ]);
+
+          // Set chart options
+          var options = {'title':'How Much Pizza I Ate Last Night',
+                         'width':400,
+                         'height':300};
+
+          // Instantiate and draw our chart, passing in some options.
+          var chart = new google.visualization.PieChart(document.getElementById('myChart'));
+          chart.draw(data, options);
         }
+        // console.log(attrs.type)
+        // scope.height = attrs.height;
+        // scope.width = attrs.width;
+
+        // if(attrs.type == undefined){
+        //   return;
+        // } else if(attrs.type == 'bubbleChart'){
+        //   var myBubbleChart = new Chart(element, controller.bubbleChartCfg);
+        // } else if(attrs.type == 'barChart'){
+        //   var myBubbleChart = new Chart(element, controller.barChartCfg);
+        // }
       }
     }
   }

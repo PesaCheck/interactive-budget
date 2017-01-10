81821564fe621e0af3e2f0a303bbee3cd7df45ea
Brian Wachanga <brian@talicraft.com>
2017-01-10 06:20:05 +0300

    Embedded tweets

--- docs/js/pesacheck.js
+++ docs/js/pesacheck.js
@@ -1,3 +1,35 @@
+pesacheck.factory("$Story", [
+  "$firebaseObject","$firebaseArray",
+  function($firebaseObject, $firebaseArray){
+    var self = this;
+    var stories = []
+    angular.extend(self, {
+      save: function(story){
+        if(typeof(story) != "object"){
+          console.log("illegal save. cancelled request")
+          return
+        }
+        return self.fetchAll().$add(story)
+      },
+      fetchAll: function(){
+        if(stories.length < 1){
+          var ref = firebase.database().ref();
+          stories = $firebaseArray(ref.child('pesacheck').child('stories'));
+        }
+        return stories;
+      },
+      get: function(id){
+        if(id == null){ return null;}
+        var ref = firebase.database().ref().child('pesacheck').child('stories').child(id);
+
+        return $firebaseObject(ref);
+      }
+    });
+
+    return self;
+  }
+])
+
 pesacheck.controller("embeddedCtrl", [
   "$stateParams",
   function($stateParams){
@@ -630,35 +662,3 @@ pesacheck.directive("bgImage", [
     }
   }
 ]);
-
-pesacheck.factory("$Story", [
-  "$firebaseObject","$firebaseArray",
-  function($firebaseObject, $firebaseArray){
-    var self = this;
-    var stories = []
-    angular.extend(self, {
-      save: function(story){
-        if(typeof(story) != "object"){
-          console.log("illegal save. cancelled request")
-          return
-        }
-        return self.fetchAll().$add(story)
-      },
-      fetchAll: function(){
-        if(stories.length < 1){
-          var ref = firebase.database().ref();
-          stories = $firebaseArray(ref.child('pesacheck').child('stories'));
-        }
-        return stories;
-      },
-      get: function(id){
-        if(id == null){ return null;}
-        var ref = firebase.database().ref().child('pesacheck').child('stories').child(id);
-
-        return $firebaseObject(ref);
-      }
-    });
-
-    return self;
-  }
-])
